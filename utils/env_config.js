c1eda14ea100cd73fcd0cf81aaea86825e370908
yash sahu <sahuyash1103@gmail.com>
2022-12-16 20:56:58 +0530

    fixed environment veriable name

--- utils/env_config.js
+++ utils/env_config.js
@@ -3,8 +3,8 @@ const config = require("config");
 const node_env = config.get("node_env") || process.env.NODE_ENV;
 const port = config.get("port") || process.env.PORT;
 const firebase_admin_key_path =
-  config.get("firebase_admin_key") ||
-  process.env.FIREBASE_ADMIN_KEY ||
+  config.get("firebase_admin_key_path") ||
+  process.env.FIREBASE_ADMIN_KEY_PATH ||
   "../firebase-admin-key.json";
 
 module.exports = {
